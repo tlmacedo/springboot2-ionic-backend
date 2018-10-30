381daa483c6a3c693c0ddf7e4450208fe488613e
Thiago Macedo <tl.macedo@hotmail.com>
2018-10-30 14:04:50 -0400

    Testando a primeira classe de dominio que foi a categoria

--- src/main/java/com/thiagomacedo/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/thiagomacedo/cursomc/resources/CategoriaResources.java
@@ -1,10 +1,15 @@
 package com.thiagomacedo.cursomc.resources;
 
+import com.thiagomacedo.cursomc.domain.Categoria;
+import org.hibernate.validator.constraints.EAN;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -13,8 +18,13 @@ public class CategoriaResources implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "REST está funcionando!";
+    public List<Categoria> listar() {
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+        return lista;
     }
 
 }
