1b82b1a2f069903aca095992fe2d9042ea3c2b84
Thiago Macedo <tl.macedo@hotmail.com>
2018-10-31 15:37:56 -0400

    ItemPedido e ItemPedidoPK

--- src/main/java/com/thiagomacedo/cursomc/domain/Pedido.java
+++ src/main/java/com/thiagomacedo/cursomc/domain/Pedido.java
@@ -3,6 +3,8 @@ package com.thiagomacedo.cursomc.domain;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Pedido implements Serializable {
@@ -23,6 +25,9 @@ public class Pedido implements Serializable {
     @ManyToOne
     @JoinColumn(name = "endereco_de_entrega_id")
     private Endereco enderecoDeEntrega;
+
+    @OneToMany(mappedBy = "id.pedido")
+    private Set<ItemPedido> itens = new HashSet<>();
 
     public Pedido() {
     }
@@ -72,6 +77,14 @@ public class Pedido implements Serializable {
 
     public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
         this.enderecoDeEntrega = enderecoDeEntrega;
+    }
+
+    public Set<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(Set<ItemPedido> itens) {
+        this.itens = itens;
     }
 
     @Override
